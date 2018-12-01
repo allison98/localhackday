d9826aeac215aaf76014161779f3c8ef848ae3c7
Binte Hassan <samiha.bintehassan@move.com>
2018-12-01 15:30:09 -0800

    can open tabs from variable

--- get_started_complete/background.js
+++ get_started_complete/background.js
@@ -5,21 +5,30 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
+  // chrome.storage.sync.set({color: '#3aa757'}, function() {
+  //   console.log('The color is green.');
+  // });
 
   // Called when the user clicks on the browser action.
   chrome.browserAction.onClicked.addListener(function(tab) {
     // Send a message to the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
+        let activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+        //create a new tab
+        let setTab = ["http://facebook.com","http://google.com"];
+        setTab.forEach(function (openTab) {
+            chrome.tabs.create({"url": openTab});
+        })
     });
-  });
 
-  //create a new tab
-  chrome.tabs.create({"url": "http://google.com"});
+    //sends an alert with the current url
+      chrome.tabs.getSelected(null, function (tab) {
+          let tablink = tab.url;
+          alert(tablink);
+          console.log(tablink);
+      });
+  });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
--- get_started_complete/content.js
+++ get_started_complete/content.js
@@ -1,12 +1,12 @@
 // content.js
 
 'use strict';
-alert("Hello from your Chrome extension!");
-
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "clicked_browser_action" ) {
+            alert("Hello from your Chrome extension!");
+
             var firstHref = $("a[href^='http']").eq(0).attr("href");
 
             console.log(firstHref);
